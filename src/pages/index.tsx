a7c252de36f38ec0fe7db7c84f50f6c00bf80f73
Jjanisruduks <janis.ruduks157@gmail.com>
2023-07-27 15:06:49 +0300

    Updated style and removed random image
    Added share twitter share button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { Layout, Typography, DatePicker, Button, Card, Space } from 'antd';
 import type { DatePickerProps } from 'antd/es/date-picker';
 import dayjs from 'dayjs';
 import axios from 'axios';
+import { Layout, Typography, DatePicker, Button, Card } from 'antd';
+import { TwitterShareButton, TwitterIcon } from 'react-share';
 import { useState } from 'react';
 
 const { Header, Content } = Layout;
@@ -11,8 +12,8 @@ export default function Home() {
   const [date, setDate] = useState('');
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [image, setImage] = useState('');
   const [birthdate, setBirthdate] = useState('');
+  const shareUrl = "https://birthday-florida-man.vercel.app/";
 
   const onChange: DatePickerProps['onChange'] = (date, dateString) => {
     const formattedDate = dayjs(`2023-${dateString.slice(5)}`).format('MMMM D');
@@ -26,7 +27,6 @@ export default function Home() {
       const { title, description } = response.data;
       setTitle(title);
       setDescription(description);
-      setImage(`https://source.unsplash.com/random/?florida,man&t=${Date.now()}`);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -36,12 +36,18 @@ export default function Home() {
     <main>
       <Layout>
         <Header className='bg-black p-3'>
-          <Title level={2} style={{color: 'white'}}>🐊birthday.florida.man</Title>
+          <Title level={3} style={{ color: 'white' }}>🐊birthday.florida.man</Title>
         </Header>
         <Content className="m-3 flex flex-col items-center justify-center">
-          <Title level={2} keyboard={true}>What is your birthday Florida man?</Title>
+          <Title
+            className="text-center"
+            level={2}
+            keyboard={true}
+          >
+            What is your birthday Florida man?
+          </Title>
           <Title level={3}>Enter your birthdate to find out!</Title>
-          
+
           <div className="gap-3 flex">
             <DatePicker onChange={onChange} />
             <Button onClick={handleSearch} className='bg-white'>
@@ -50,11 +56,26 @@ export default function Home() {
           </div>
         </Content>
         <Content className="p-3 flex flex-col items-center justify-center">
-          {title && description && image && birthdate && (
-            <Card style={{ width: 400 }} title={birthdate} cover={<img alt='florida man' src={image} />} className=" border-slate-100 shadow-md">
-              <p>{title}</p>
-              <br></br>
-              <p>{description}</p>
+          {title && description && birthdate && (
+            <Card
+              style={{ width: 400 }}
+              title={birthdate}
+              className="border-slate-100 shadow-md"
+              actions={[
+                <TwitterShareButton
+                  key="share"
+                  className='m-3'
+                  url={shareUrl}
+                  title={"My birthday Florida man is:\n" + title}
+                >
+                  <TwitterIcon size={32} round />
+                  <p>Share</p>
+                </TwitterShareButton>
+              ]}
+            >
+              <p className='font-semibold'>{title}</p>
+              <br />
+              <p className="font-thin">{description}</p>
             </Card>
           )}
         </Content>
